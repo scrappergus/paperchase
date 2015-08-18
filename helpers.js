1e981944a9a8aabe9dc821c67071d56a323543fb
Jessica Lawshe <jessica@jlawshe.com>
2015-08-18 17:03:39 -0500

    sort in helpers query

--- helpers.js
+++ helpers.js
@@ -1,11 +1,13 @@
 if (Meteor.isClient) {
 	Template.Archive.helpers({
 		volumes: function(){
-			var vol = volumes.find({}).fetch();
-			var iss = issues.find({}).fetch();
+			var vol = volumes.find({},{sort : {volume:-1}}).fetch();
+			var iss = issues.find({},{sort : {page_start:1}}).fetch();
 			var res = Meteor.organize.issuesIntoVolumes(vol,iss);
 			// console.log('res = ');console.log(res);
 			return res;
 		}
 	});
 }
+
+// TODO: Figure out better sorting of issues. They may not have numbers. Right now the issues are sorted by the first page. 
