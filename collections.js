1120f306d123ce5e420d4ce2b90a3410bcb622b5
Jessica Lawshe <jessica@jlawshe.com>
2015-08-18 16:41:05 -0500

    publish & subscribe collections
    sort collections by volume
    
    global variable of collections, need to subscribe on the archive page
    instead of globally

--- collections.js
+++ collections.js
@@ -1,2 +1,16 @@
 volumes = new Mongo.Collection('volumes');
 issues = new Mongo.Collection('issues');
+
+if (Meteor.isServer) {
+  Meteor.publish('volumes', function () {
+    return volumes.find({},{sort : {volume:-1}});
+  });
+  Meteor.publish('issues', function () {
+    return issues.find({},{sort : {volume:1}});
+  });
+}
+if (Meteor.isClient) {
+	//TODO: remove global subscribe to collections
+	Meteor.subscribe('volumes');
+	Meteor.subscribe('issues');
+}
