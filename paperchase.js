8ca13efcdbd6c02594f7b672c64a3953c1022b25
Jessica Lawshe <jessica@jlawshe.com>
2015-08-18 17:46:59 -0500

    route for issue
    2 parameters: volume and issue
    TODO: fix query for issue data

--- paperchase.js
+++ paperchase.js
@@ -11,6 +11,28 @@ if (Meteor.isClient) {
             layoutTemplate: 'Visitor',
         });
 
+    Router.route('/volume/:volume/issue/:issue', { 
+            name: 'issue',
+            layoutTemplate: 'Visitor',
+            waitOn: function(){
+                return[
+                    Meteor.subscribe('issues')
+                ]
+            },
+            data: function(){
+                if(this.ready()){
+                    iss = this.params.issue;
+                    vol = this.params.volume;
+                    // console.log(vol);console.log(iss);console.log(issues.find().fetch());
+                    var issueData = issues.findOne({'issue':iss,'volume':vol});
+                    // console.log('issueData');console.log(issueData);
+                    return {
+                        issue: issueData
+                    };
+                }
+            }
+        });
+
 
     Router.route('/admin', {
             layoutTemplate: 'admin',
