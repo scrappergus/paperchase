1d21ad5ec62f272addaf0111f862770a0521de02
Jessica Lawshe <jessica@jlawshe.com>
2015-08-19 11:45:58 -0500

    new collections - articles, articleTypes
    articles will store all article metadata, along with an object for
    article_type
    
    publish articles collection, sort by page_start. TODO: add a field for
    sorting not based on page number, online only articles might not have
    page counts

--- collections.js
+++ collections.js
@@ -1,5 +1,7 @@
 volumes = new Mongo.Collection('volumes');
 issues = new Mongo.Collection('issues');
+articles = new Mongo.Collection('articles');
+articleTypes = new Mongo.Collection('articleTypes'); //when saving an article query this db and add the name, short_name and id as an object to the article
 
 if (Meteor.isServer) {
   Meteor.publish('volumes', function () {
@@ -7,6 +9,9 @@ if (Meteor.isServer) {
   });
   Meteor.publish('issues', function () {
     return issues.find({},{sort : {volume:1}});
+  });
+  Meteor.publish('articles', function () {
+    return articles.find({},{sort : {page_start:1}});
   });
 }
 if (Meteor.isClient) {
