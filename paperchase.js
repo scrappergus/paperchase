6882b8e84927734e0b1cbd0802989e1c29625521
Jessica Lawshe <jessica@jlawshe.com>
2015-08-19 14:22:32 -0500

    Article Route with data

--- paperchase.js
+++ paperchase.js
@@ -27,13 +27,33 @@ if (Meteor.isClient) {
                     //get issue metadata
                     var issueData = issues.findOne({'issue': iss, 'volume': vol});
                     //get articles in issue
-                    var issueArticles = articles.find({'issue_id':issueData._id}).fetch();
-                    //group articles by type
+                    var issueArticles = articles.find({'issue_id':issueData._id},{sort : {page_start:1}}).fetch();
+                    //test for start of article type
                     issueArticles = Meteor.organize.groupArticles(issueArticles);
                     issueData['articles'] = issueArticles;
-                    //console.log(issueData);
+                    // console.log(issueData);
                     return {
                         issue: issueData
+                    };
+                }
+            }
+        });
+
+    Router.route('/article/:_id', { 
+            name: 'Article',
+            layoutTemplate: 'Visitor',
+            waitOn: function(){
+                return[
+                    Meteor.subscribe('articles')
+                ]
+            },
+            data: function(){
+                if(this.ready()){
+                    var id = this.params._id;
+                    var article = articles.findOne({'_id': id});
+                    // console.log('article = ');console.log(article);
+                    return {
+                        article: article
                     };
                 }
             }
