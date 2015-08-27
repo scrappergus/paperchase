bf101640bbfab21184eb8bd6ee947a591a230f12
Jessica Lawshe <jessica@jlawshe.com>
2015-08-27 16:36:02 -0500

    Admin User Creation on Server - Roles
    user.roles does not get added in Accounts.createUser(). Must use
    Roles.addUsersToRoles()

--- server/userMethods.js
+++ server/userMethods.js
@@ -1,13 +1,14 @@
 
 Meteor.methods({
     addUser: function (user) {
-      console.log('..addUser');
       var loggedInUser = Meteor.user()
 
       if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['admin'])) {
         throw new Meteor.Error(403, "Access denied")
       }
 
-      return Accounts.createUser(user);
+      var id = Accounts.createUser(user);
+      Roles.addUsersToRoles(id, user.roles);
+      return id;
     }
 })
