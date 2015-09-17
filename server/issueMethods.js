905b5b5a93132abbcdc1e7ff93c5568fd9b26db5
Jessica Lawshe <jessica@jlawshe.com>
2015-09-17 18:15:09 -0500

    issue and volume methods - add to db

--- server/issueMethods.js
+++ server/issueMethods.js
@@ -1,5 +1,18 @@
 Meteor.methods({
 	addIssue: function(issueData){
+		//check if volume exists, if not add
+		var vol;
+		vol = volumes.findOne({'volume':issueData['volume']});
+		if(!vol){
+			Meteor.call('addVolume',issueData['volume'], function(error,_id){
+				if(error){
+					console.log('ERROR: ' + error.message);
+				}
+			});
+		}
 		return issues.insert(issueData);
+	},
+	addVolume: function( vol){
+		return volumes.insert({'volume':vol});
 	}
 });
