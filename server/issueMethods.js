ac25d44ce7eb6e679bf284e8edd25a06f432c9d3
Jessica Lawshe <jessica@jlawshe.com>
2015-09-17 18:37:16 -0500

    issue method - add issue, double check if issue exists

--- server/issueMethods.js
+++ server/issueMethods.js
@@ -1,8 +1,18 @@
 Meteor.methods({
 	addIssue: function(issueData){
 		//check if volume exists, if not add
-		var vol;
+		var vol, 
+			iss, 
+			issueId;
 		vol = volumes.findOne({'volume':issueData['volume']});
+		//double check that issue does not exit 
+		iss = issues.findOne({'volume':issueData['volume'],'issue':issueData['issue']});
+
+		if(!iss){
+			issueId = issues.insert(issueData);
+		}else{
+			issueId = iss['_id'];
+		}
 		if(!vol){
 			Meteor.call('addVolume',issueData['volume'], function(error,_id){
 				if(error){
@@ -10,7 +20,7 @@ Meteor.methods({
 				}
 			});
 		}
-		return issues.insert(issueData);
+		return issueId;
 	},
 	addVolume: function( vol){
 		return volumes.insert({'volume':vol});
