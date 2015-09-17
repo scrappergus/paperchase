99c3d11f275b5fa911e21f85c63070e5e2fc4855
Jessica Lawshe <jessica@jlawshe.com>
2015-09-17 18:15:33 -0500

    initiate dropdown in menu

--- client/js/template.js
+++ client/js/template.js
@@ -1,3 +1,9 @@
 Template.adminArticleXmlIntake.rendered = function(){
     Session.set('fileNameXML','');
 }
+Template.Admin.rendered = function(){
+	$('.dropdown-button').dropdown();
+}
+Template.Visitor.rendered = function(){
+	$('.dropdown-button').dropdown();
+}
