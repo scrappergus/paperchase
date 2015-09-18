b19d2d15d426aa431ff60d0e0b4b2e83998c99b2
Jessica Lawshe <jessica@jlawshe.com>
2015-09-18 12:11:17 -0500

    removed not working

--- client/js/template.js
+++ client/js/template.js
@@ -1,9 +1,3 @@
 Template.adminArticleXmlIntake.rendered = function(){
     Session.set('fileNameXML','');
 }
-Template.Admin.rendered = function(){
-	$('.dropdown-button').dropdown();
-}
-Template.Visitor.rendered = function(){
-	$('.dropdown-button').dropdown();
-}
