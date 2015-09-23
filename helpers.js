ff666effa220df5fcf7a0c3f8cbe4ed6d4359300
Jessica Lawshe <jessica@jlawshe.com>
2015-09-23 12:32:36 -0500

    article partial for - title, authors, pages, dates, type, references
    new helper for collection date, which does not have a day

--- helpers.js
+++ helpers.js
@@ -28,6 +28,9 @@ if (Meteor.isClient) {
 	Template.registerHelper('articleDate', function(date) {
 		return moment(date).format('MMMM D, YYYY');
 	});	
+	Template.registerHelper('collectionDate',function(date) {
+		return moment(date).format('MMMM YYYY');
+	});
 	Template.registerHelper('equals', function (a, b) {
 		return a === b;
     });	
