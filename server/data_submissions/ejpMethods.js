9f0c71cb9a183c8b6162e0236bd0a55b4c54bcef
Jessica Lawshe <jessica@jlawshe.com>
2015-09-24 18:17:08 -0500

    EJP - cookies in header type fix
    as an array of strings instead of an object

--- server/data_submissions/ejpMethods.js
+++ server/data_submissions/ejpMethods.js
@@ -18,37 +18,20 @@ Meteor.methods({
 				'Connection' : 'Keep-Alive',
 				'User-Agent' : agent
           	}
-		});		
+		});
 		if(res.statusCode == 200){
 			//find ms_id_key in the login form
 			var resHtml = res.content;
-			// console.log(resHtml.find('ms_id_key'));
-			// console.log(resHtml.lastIndexOf('id=\'ms_id_key\'')+1);
 			var keyStart = resHtml.lastIndexOf('id=\'ms_id_key\'')+22;
 			var keyEnd = keyStart + 28;
 			ms = resHtml.substring(keyStart, keyEnd);
-			// console.log('ms_id_key=====' + ms + '=====');
 
-			console.log('res HEADERS =====');
-			console.log(res.headers);
+			//SET COOKIES -- set the cookie_value to the response's set-cookie
+			cookie_value = res['headers']['set-cookie'];
 
-			//SET COOKIES
-			var cookieObject = {};
-			if(res['headers']['set-cookie']){
-				var cookies = res['headers']['set-cookie'];
-				for(var c = 0 ; c < cookies.length ; c++){
-					var cookiePieces = cookies[c].split('=');
-					// console.log('cookie  =====');
-					// console.log(cookies[c]);
-					cookieObject[cookiePieces[0]] = cookiePieces[1];
-				}
-			}
-			console.log(cookieObject);
-
-		
 			//LOGIN and GET Manuscripts
 			var manuscriptsPath = 'cgi-bin/main.plex?form_type=ndt_folder&j_id=459&ms_id_key=684ftdWXpqQ0cuPXznOnZIQ2SiQ&ft_key=NQcbqxCKr20xjQr3VmgxSg&is_open_2000=1&folder_id=2000&is_open_view_2000=11&role=20';
-			Meteor.http.post(requestURL + manuscriptsPath, {
+			var login_res = Meteor.http.post(requestURL + manuscriptsPath, {
 				// auth : authCredString,
 				params: {
 					timeout: 30000,
@@ -62,26 +45,12 @@ Meteor.methods({
 					'Accept' : '*/*',
 					'Connection' : 'Keep-Alive',
 					'User-Agent' : agent,
-					'Cookie' : cookieObject
+					'cookie' : cookie_value
 	          	}
-			},function(error,r){
-				if(error){
-					console.log('error');
-					console.log(error);
-				}else{
-					console.log(r);
-				}
 			});
-
-
-			//GET - get manuscripts
-			// if (result.statusCode == 200) {
-			// 	// console.log('login HEADERS =====');
-			// 	console.log('login res');
-			// 	console.log(result);
-			// }		
+			if(login_res.statusCode == 200){
+				console.log(login_res.content);
+			}
 		}
-
-
 	}
 });
