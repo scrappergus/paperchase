71da0e41dce24d2a8a5feb54cd0cd5d99d3110be
Jessica Lawshe <jessica@jlawshe.com>
2015-10-02 17:15:33 -0500

    Edit Article - Starting to update article doc
    Only updating:
    Feature
    Advance
    Authors
    Affiliations
    
    TODO: Complete add affiliation to author

--- client/js/template.js
+++ client/js/template.js
@@ -5,15 +5,25 @@ Template.AdminArticle.rendered = function(){
 			Meteor.adminArticle.updateAffiliationsOrder();
 		}
 	});
+
+	$('.dropdown-button').dropdown({
+	  	inDuration: 300,
+	  	outDuration: 225,
+	  	constrain_width: false, // Does not change width of dropdown to that of the activator
+	  	gutter: 0, // Spacing from edge
+	  	belowOrigin: false, // Displays dropdown below the button
+	  	alignment: 'left' // Displays dropdown with edge aligned to the left of button
+	}
+  );
 }
 Template.adminArticleXmlIntake.rendered = function(){
-    Session.set('fileNameXML','');
+	Session.set('fileNameXML','');
 }
 Template.adminIssue.rendered = function(){
 	$('.datepicker').pickadate({
-    	selectMonths: true,
+		selectMonths: true,
 		selectYears: 1
-  	});
+	});
 }
 Template.AdminDataSubmissions.rendered = function(){
 	$('select').material_select();
