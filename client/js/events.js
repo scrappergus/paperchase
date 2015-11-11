22655c22e8bdad334558a24fa1c7fa7ebe53f1b5
Jessica Lawshe <jessica@jlawshe.com>
2015-11-10 22:09:27 -0600

    scrollTop only when element exists

--- client/js/events.js
+++ client/js/events.js
@@ -206,7 +206,7 @@ Template.AdminArticleForm.events({
 		article.authors.push(newAuthor);
 
 		//scroll to new affiliation <li>
-		if($('.author-li:last-child')){
+		if($('.author-li:last-child').length != 0){
 			$('html, body').animate({
 				scrollTop: $('.author-li:last-child').find('input').position().top
 			}, 500);
@@ -249,7 +249,7 @@ Template.AdminArticleForm.events({
 
 		// scroll to new affiliation <li>
 		// TODO: when no affiliations, get error: Uncaught TypeError: Cannot read property 'top' of undefined (for last-child)
-		if($('.affiliation-li:last-child')){
+		if($('.affiliation-li:last-child').length != 0){
 			$('html, body').animate({
 				scrollTop: $('.affiliation-li:last-child').find('input').position().top
 			}, 500);
@@ -313,9 +313,11 @@ Template.AdminArticleForm.events({
 		}
 		article.keywords.push('');
 		Session.set('article',article);
-		$('html, body').animate({
-			scrollTop: $('.kw-li:last-child').find('input').position().top
-		}, 500);
+		if($('.kw-li:last-child').length != 0){
+			$('html, body').animate({
+				scrollTop: $('.kw-li:last-child').find('input').position().top
+			}, 500);
+		}
 	},
 	'click .remove-kw': function(e,t){
 		e.preventDefault();
