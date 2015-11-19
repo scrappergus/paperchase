b94ca571bbaa344d43e58b5b37fdb328404ee26b
Jessica Lawshe <jessica@jlawshe.com>
2015-11-19 11:43:09 -0600

    Get PMID from article data
    Using all values in article object (just reference right now) query
    PubMed for PMID.
    TODO: Verify PMID is correct with a 2nd query for title match.

--- server/methods/ncbiMethods.js
+++ server/methods/ncbiMethods.js
@@ -89,5 +89,118 @@ Meteor.methods({
 			}
 		});
 		return fut.wait();
-	}
-})
+	},
+	getPubMedId: function(article){
+		// console.log('...getPubMedId ');
+		var fut = new future();
+		// console.log(article.title);
+		// using title, authors, etc query PubMed to get article PMID, which will then be the URL for the article
+		if(article.title){
+			// title = for double check
+			// require title because this is what we will use to verify that the PMID retrieved is correct.
+			var pubMedUrl = 'http://www.ncbi.nlm.nih.gov/pubmed/';
+			var query = [];
+			for(var key in article){
+				var v = article[key];
+				// console.log(v);
+				if(typeof v == Array){
+					v = v.join('+');
+				}
+
+				query.push(v);
+			}
+			query = query.join('+').replace(/\s+/g, '+');;
+			// console.log('query');console.log(query);
+			Meteor.http.get(pubMedUrl + '?term=' + query, function(error,result){
+				if(error){
+					console.log('error');
+					console.log(error);
+				}
+				if(result){
+					var pmid = '';
+					var resultHtml = result.content;
+					var doc = new dom().parseFromString(resultHtml);
+					var pmidElement = doc.getElementById('absid');
+					if(pmidElement){
+						for(var attr=0 ; attr < pmidElement.attributes.length ; attr++){
+							// if()
+							// console.log(pmid.attributes[attr].localName);
+							if(pmidElement.attributes[attr].localName == 'value' && pmidElement.attributes[attr].nodeValue){
+								pmid = pmidElement.attributes[attr].nodeValue;
+								// console.log(pmid);
+								fut['return'](pmid);
+							}
+						}
+						// TODO:
+						// Verify that PMID is correct
+						// Meteor.http.get(pubMedUrl + pmid, function(error,result){
+						// 	if(error){
+						// 		console.log('error');
+						// 		console.log(error);
+						// 	}
+						// 	if(result){
+						// 		// title match check
+						// 		var resultTitle = '';
+						// 		var resultHtml = result.content;
+						// 		var doc = new dom().parseFromString(resultHtml);
+						// 		var resultTitleElement = doc.getElementsByTagName('title');
+						// 		var resultTitle = resultTitleElement[0].firstChild.nodeValue.replace('.  - PubMed - NCBI\n','');
+						// 		// console.log(resultTitleElement[0].firstChild.nodeValue.replace('.  - PubMed - NCBI\n',''));
+						// 		// console.log(resultTitleElement[0].firstChild[0].nodeValue.replace('.  - PubMed - NCBI\n',''));
+						// 		// resultTitle = resultTitleElement.childNodes[0].nodeValue;
+						// 		// console.log('resultTitle');
+						// 		// console.log(resultTitle);
+						// 		if(article.artice_title == resultTitle){
+						// 			console.log('MATCH! = ' + pmid);
+						// 			fut['return'](pmid);
+						// 			// return pmid;
+						// 		}else{
+						// 			fut['return'](false);
+						// 			// return false;
+						// 		}
+
+						// 	}
+						// });
+					}
+				}else{
+					// return false;
+					fut['return'](false);
+				}
+			});
+		}
+		return fut.wait();
+	},
+	// verifyPubMedId: function(pmid,title){
+	// 	console.log('..verifyPubMedId = ' + pmid);
+	// 	var fut = new future();
+	// 	var pubMedUrl = 'http://www.ncbi.nlm.nih.gov/pubmed/';
+	// 	// after querying PubMed for ID, verify that titles match
+	// 	Meteor.http.get(pubMedUrl + pmid, function(error,result){
+	// 		if(error){
+	// 			console.log('error');
+	// 			console.log(error);
+	// 		}
+	// 		if(result){
+	// 			// title match check
+	// 			var resultTitle = '';
+	// 			var resultHtml = result.content;
+	// 			var doc = new dom().parseFromString(resultHtml);
+	// 			var resultTitleElement = doc.getElementsByTagName('title');
+	// 			var resultTitle = resultTitleElement[0].firstChild.nodeValue.replace('.  - PubMed - NCBI\n','');
+	// 			// console.log(resultTitleElement[0].firstChild.nodeValue.replace('.  - PubMed - NCBI\n',''));
+	// 			// console.log(resultTitleElement[0].firstChild[0].nodeValue.replace('.  - PubMed - NCBI\n',''));
+	// 			// resultTitle = resultTitleElement.childNodes[0].nodeValue;
+	// 			// console.log('resultTitle');
+	// 			// console.log(resultTitle);
+	// 			if(title == resultTitle){
+	// 				console.log('MATCH! = ' + pmid);
+	// 				fut['return'](true);
+	// 			}else{
+	// 				fut['return'](false);
+	// 			}
+
+	// 		}
+	// 	});
+	// 	return fut.wait();
+	// }
+});
