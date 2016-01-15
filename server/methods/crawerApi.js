624f3701aabc1666e5d68ef9cc3e01e8fd1e9c64
Jessica Lawshe <jessica@jlawshe.com>
2016-01-15 09:44:50 -0600

    DOI status via crawler - check for doc existence & variable name fixed

--- server/methods/crawerApi.js
+++ server/methods/crawerApi.js
@@ -6,24 +6,27 @@ Meteor.methods({
 		Meteor.http.get(requestURL, function(error,result){
 			if(error){
 				console.error(error);
-				fut['throw'](err);
-				throw new Meteor.Error(503, 'ERROR: DOI Registered Check' , err);
+				fut['throw'](error);
+				throw new Meteor.Error(503, 'ERROR: DOI Registered Check' , error);
 			}else if(result){
 				// combine with articles DB
 				var articlesDoiList = JSON.parse(result.content);
 				for(var a=0 ; a<articlesDoiList.length ; a++){
 					var pii = articlesDoiList[a]['pii'];
 					var articleInfo = articles.findOne({'ids.pii': pii},{'dates.epub' : 1, 'ids' : 1});
-					if(articleInfo.dates && articleInfo.dates.epub){
-						articlesDoiList[a]['epub'] = articleInfo.dates.epub;
+					if(articleInfo){
+						if(articleInfo.dates && articleInfo.dates.epub){
+							articlesDoiList[a]['epub'] = articleInfo.dates.epub;
+						}
+						if(articleInfo['ids']['pmc']){
+							articlesDoiList[a]['pmc'] = articleInfo['ids']['pmc'];
+						}
+						if(articleInfo['ids']['pmid']){
+							articlesDoiList[a]['pmid'] = articleInfo['ids']['pmid'];
+						}
+					}else{
+						console.log('DB Missing PII ' + pii);
 					}
-					if(articleInfo['ids']['pmc']){
-						articlesDoiList[a]['pmc'] = articleInfo['ids']['pmc'];
-					}
-					if(articleInfo['ids']['pmid']){
-						articlesDoiList[a]['pmid'] = articleInfo['ids']['pmid'];
-					}
-
 				}
 
 				fut['return'](articlesDoiList);
