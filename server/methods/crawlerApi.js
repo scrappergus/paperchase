bc4d5975be4b08dbcb303c9f9cd463bb9ca6c37d
Jessica Lawshe <jessica@jlawshe.com>
2016-01-19 16:23:46 -0600

    Removed adding data to CrossRef, now it is returned from API
    Paperchase data returned from Crawler API

--- server/methods/crawlerApi.js
+++ server/methods/crawlerApi.js
@@ -40,6 +40,7 @@ Meteor.methods({
 		// console.log('..getAllArticlesDoiStatus');
 		var fut = new future();
 		var requestURL =  journalConfig.findOne().api.crawler + '/doi_status/' + journalConfig.findOne().journal.short_name;
+		// console.log('requestURL = ' + requestURL);
 		var registerURL =  journalConfig.findOne().api.doi + journalConfig.findOne().journal.short_name;
 		Meteor.http.get(requestURL, function(error,result){
 			if(error){
@@ -49,26 +50,6 @@ Meteor.methods({
 			}else if(result){
 				// combine with articles DB
 				var articlesDoiList = JSON.parse(result.content);
-				for(var a=0 ; a<articlesDoiList.length ; a++){
-					var pii = articlesDoiList[a]['pii'];
-					// console.log('..PII ' + pii);
-					var articleInfo = articles.findOne({'ids.pii': pii},{'dates.epub' : 1, 'ids' : 1});
-					if(articleInfo){
-						if(articleInfo.dates && articleInfo.dates.epub){
-							articlesDoiList[a]['epub'] = articleInfo.dates.epub;
-						}
-						if(articleInfo['ids']['pmc']){
-							articlesDoiList[a]['pmc'] = articleInfo['ids']['pmc'];
-						}
-						if(articleInfo['ids']['pmid']){
-							articlesDoiList[a]['pmid'] = articleInfo['ids']['pmid'];
-						}
-						articlesDoiList[a].doiRegisterUrl = registerURL;
-					}else{
-						console.log('DB Missing PII ' + pii);
-					}
-				}
-
 				fut['return'](articlesDoiList);
 			}
 		});
