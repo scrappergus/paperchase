bf27b0b5a38b2783738adec667404e78c64aee7a
Jessica Lawshe <jessica@jlawshe.com>
2016-01-20 13:35:39 -0600

    Removed commented outdated code

--- server/methods/batchMethods.js
+++ server/methods/batchMethods.js
@@ -13,31 +13,6 @@ Meteor.methods({
 				// console.log('-- PII: ' + articlesList[a]['ids']['pii']);
 				// get XML and update DB
 				var articleXML = xmlUrl + articlesList[a]['ids']['paperchase_id'] + '.xml';
-				// Meteor.call('fileExistsOnS3', articleXML, function(errExists,exists){
-				// 	if(errExists){
-				// 		console.error('File Does Not Exist', errExists);
-				// 	}else if(exists){
-				// 		Meteor.call('parseXmlAfterUpload',articleXML, function(error,result){
-				// 			if(error){
-				// 				console.error('parseXmlAfterUpload',error);
-				// 			}else if(result){
-				// 				// maintain PII when batch updating via XML
-				// 				var articleInfo = articles.findOne(articlesList[a]['_id']);
-				// 				if(articleInfo.ids && articleInfo.ids.pii){
-				// 					result.ids.pii = articleInfo.ids.pii;
-				// 				}
-				// 				result.ids.paperchase_id = articleInfo.ids.paperchase_id;
-				// 				Meteor.call('updateArticle',articlesList[a]['_id'], result,function(articleUpdateError,articleUpdate){
-				// 					if(articleUpdateError){
-				// 						console.error('Could not update article doc: ' + articlesList[a]['_id'], articleUpdateError);
-				// 					}else{
-				// 						console.log('  '+articlesList[a]['_id'] + ' Updated');
-				// 					}
-				// 				})
-				// 			}
-				// 		});
-				// 	}
-				// });
 				Meteor.call('parseXmlAfterUpload',articleXML, function(error,result){
 					if(error){
 						console.error('parseXmlAfterUpload',error);
