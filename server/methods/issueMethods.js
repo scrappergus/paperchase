ce8c798acca50346fdebdead1764f9b626d293d4
Jessica Lawshe <jessica@jlawshe.com>
2016-01-22 14:34:22 -0600

    Archive, addIssue, addVolume, addIssueToVolume
    archive
    ———
    Combine Volumes and Issues collections, as well as cover image path.
    
    addIssue
    ————
    issue field no longer integer. And when adding a new volume, include
    the Issue MongoID to insert into volume doc.
    
    addVolume
    ——————
    Include issues field now
    
    addIssueToVolume
    ————————
    when adding a new issue, append MongoID to end of issues list in volume

--- server/methods/issueMethods.js
+++ server/methods/issueMethods.js
@@ -1,10 +1,48 @@
 Meteor.methods({
+	archive: function(articleIssueId){
+		//combine vol and issue collections
+
+		var issuesList = issues.find({},{}).fetch();
+		var volumesList = volumes.find({},{sort : {volume:-1}}).fetch();
+		var issuesObj = {};
+		for(var i=0 ; i<issuesList.length ; i++){
+			issuesObj[issuesList[i]['_id']] = issuesList[i];
+		}
+
+		for(var v=0 ; v < volumesList.length ; v++){
+			volumesList[v]['issues_data'] = [];
+			var volumeIssues = volumesList[v]['issues'];
+			for(var vi=0 ; vi < volumeIssues.length ; vi++){
+				var volumeIssuesId = volumeIssues[vi];
+				// add cover path to issue
+				if(articleIssueId && volumeIssuesId == articleIssueId){
+					// For providing all available issues on the article form
+					issuesObj[volumeIssuesId]['selected'] = true;
+				}
+				issuesObj[volumeIssuesId]['cover'] = Meteor.issue.coverPath(issuesObj[volumeIssuesId]['volume'],issuesObj[volumeIssuesId]['issue']);
+				volumesList[v]['issues_data'].push(issuesObj[volumeIssuesId]);
+			}
+		}
+		// console.log('volumesList',volumesList);
+		return volumesList;
+	},
+	getVolume: function(volume){
+		// will return all issues and issue data. No articles.
+		var vol = volumes.findOne({volume : parseInt(volume)});
+		vol.issues_data = [];
+		var issueIds = vol.issues;
+		for(var i=0 ; i<issueIds.length ; i++){
+			var issue = issues.findOne({_id : issueIds[i]});
+			vol.issues_data.push(issue);
+		}
+		return vol;
+	},
 	addIssue: function(issueData){
 		//check if volume exists, if not add
 		var vol,
 			iss,
 			issueId;
-		issueData['issue'] = parseInt(issueData['issue']);
+		issueData['issue'] = issueData['issue'];
 		issueData['volume'] = parseInt(issueData['volume']);
 		vol = volumes.findOne({'volume':issueData['volume']});
 		//double check that issue does not exit
@@ -16,7 +54,14 @@ Meteor.methods({
 			issueId = iss['_id'];
 		}
 		if(!vol){
-			Meteor.call('addVolume',issueData['volume'], function(error,_id){
+			Meteor.call('addVolume',issueData['volume'],issueData['issue'], function(error,_id){
+				if(error){
+					console.log('ERROR: ' + error.message);
+				}
+			});
+		}else{
+			// Add issue to issue array, append to end
+			Meteor.call('addIssueToVolume',issueData['volume'],issueId, function(error,_id){
 				if(error){
 					console.log('ERROR: ' + error.message);
 				}
@@ -27,8 +72,16 @@ Meteor.methods({
 	updateIssue: function(mongoId, update){
 		return issues.update({'_id':mongoId},{$set:update})
 	},
-	addVolume: function( vol){
-		return volumes.insert({'volume':vol});
+	addVolume: function(volume,issue){
+		var insertObj = {'volume':volume};
+		if(issue){
+			insertObj['issues'] = [issue];
+		}
+		return volumes.insert(insertObj);
+	},
+	addIssueToVolume: function(volume,newIssueMongoId){
+		// console.log('..addIssueToVolume: ' + newIssueMongoId);
+		return volumes.update({volume : volume}, {$addToSet : {'issues' : newIssueMongoId}});
 	},
 	findIssueByVolIssue: function(vol, iss){
 		return issues.findOne({'volume' : vol, 'issue': iss});
@@ -36,9 +89,9 @@ Meteor.methods({
 	getIssueAndAssets: function(volume, issue){
 		// console.log('...getIssueAndAssets v = ' + volume + ', i = ' + issue);
 		var fut = new future();
-		var issueData = issues.findOne({'issue': issue, 'volume': volume});
+		var issueData = issues.findOne({'issue_linkable': issue, 'volume': parseInt(volume)});
+		// console.log('issueData',issueData);
 		issueData.cover = Meteor.issue.coverPath(volume,issue);
-		// console.log(issueData);
 		var issueArticles = Meteor.organize.getIssueArticlesByID(issueData['_id']);
 		// console.log(issueArticles);
 		// get assets
@@ -60,5 +113,5 @@ Meteor.methods({
 	},
 	getAllVolumes: function(){
 		return volumes.find().fetch();
-	}
+	},
 });
