f7fdac8def223387aff5ad067f5bc3a76536bd56
Jessica Lawshe <jessica@jlawshe.com>
2016-02-01 17:04:58 -0600

    Removing internal ID
    No longer using paperchase_id for article assets. Now using article
    Mongo ID.

--- server/methods/assetsMethods.js
+++ server/methods/assetsMethods.js
@@ -3,19 +3,15 @@ Meteor.methods({
 		console.log('... articleAssests: Mongo ID ', mongoId);
 		var article = articles.findOne({_id : mongoId});
 		var assets = {};
-		if(article.ids.paperchase_id){
-			var paperchaseId = article.ids.paperchase_id;
-			// console.log('paperchaseId',paperchaseId);
-			// XML
-			var xmlAsset = xmlCollection.findOne({'ids.paperchase_id' : paperchaseId});
-			if(xmlAsset && xmlAsset.xml_url){
-				assets.xml_url = xmlAsset.xml_url;
-			}
-			// PDF
-			var pdfAsset = pdfCollection.findOne({'ids.paperchase_id' : paperchaseId});
-			if(pdfAsset && pdfAsset.pdf_url){
-				assets.pdf_url = pdfAsset.pdf_url;
-			}
+		// XML
+		var xmlAsset = xmlCollection.findOne({'ids.mongo_id' : mongoId});
+		if(xmlAsset && xmlAsset.xml_url){
+			assets.xml_url = xmlAsset.xml_url;
+		}
+		// PDF
+		var pdfAsset = pdfCollection.findOne({'ids.mongo_id' : mongoId});
+		if(pdfAsset && pdfAsset.pdf_url){
+			assets.pdf_url = pdfAsset.pdf_url;
 		}
 		// console.log(assets);
 		return assets;
