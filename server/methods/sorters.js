b58f033fc9fefd456fd0f721a14d0a0e73457971
Jessica Lawshe <jessica@jlawshe.com>
2016-02-11 11:38:10 -0600

    Spaces to Tabs

--- server/methods/sorters.js
+++ server/methods/sorters.js
@@ -3,48 +3,48 @@ Meteor.methods({
 		// not always used for articles. for ex, use this for about and for authors sections.
 		// TODO add to the beginning of set
 
-        if(listName == 'articles') {
-            //find the position to insert at
-            article = articles.findOne({"_id": mongoId});
-            if(article) {
-                Meteor.call('sorterRemoveItem', listName, mongoId);
-                sorts = sorters.findOne({'name': listName});
-                position = sorts.order.length;
-                for(var i=0; i < sorts.order.length; i++) {
-                    match = articles.findOne({"_id":sorts.order[i], section_id:article.section_id});
-                    if(match) {
-                        position = i;
-                        i = sorts.order.length;
-                    }
-                }
+		if(listName == 'articles') {
+			//find the position to insert at
+			article = articles.findOne({"_id": mongoId});
+			if(article) {
+				Meteor.call('sorterRemoveItem', listName, mongoId);
+				sorts = sorters.findOne({'name': listName});
+				position = sorts.order.length;
+				for(var i=0; i < sorts.order.length; i++) {
+					match = articles.findOne({"_id":sorts.order[i], section_id:article.section_id});
+					if(match) {
+						position = i;
+						i = sorts.order.length;
+					}
+				}
 
-                var res = sorters.update({name : listName}, {$push : {
-                    'order': {
-                        $each: [mongoId],
-                        $position: position
-                }}});
-            }
-        } else {
-            var res = sorters.update({name : listName}, {$addToSet : {'order' : mongoId}},{upsert: true});
-        }
+				var res = sorters.update({name : listName}, {$push : {
+					'order': {
+						$each: [mongoId],
+						$position: position
+				}}});
+			}
+		} else {
+			var res = sorters.update({name : listName}, {$addToSet : {'order' : mongoId}},{upsert: true});
+		}
 
-        return res;
-    },
-    sorterRemoveItem: function(listName,mongoId){
-        var res = sorters.update({name : listName}, {$pull : {'order' : mongoId}});
 		return res;
 	},
-    batchSorterRemoveItem: function(listName,idList){
-        var removed = idList.map(function(mongoId){
-            Meteor.call('sorterRemoveItem',listName, mongoId, function(error,result){
-                if(result){
-                    return mongoId;
-                }
-            });
-        });
-        // return fut.wait();
-        return removed;
-    },
+	sorterRemoveItem: function(listName,mongoId){
+		var res = sorters.update({name : listName}, {$pull : {'order' : mongoId}});
+		return res;
+	},
+	batchSorterRemoveItem: function(listName,idList){
+		var removed = idList.map(function(mongoId){
+			Meteor.call('sorterRemoveItem',listName, mongoId, function(error,result){
+				if(result){
+					return mongoId;
+				}
+			});
+		});
+		// return fut.wait();
+		return removed;
+	},
 	updateList: function(listName, list){
 		// console.log('... sorterUpdateList = ' + listName );
 		// update sorters collection
