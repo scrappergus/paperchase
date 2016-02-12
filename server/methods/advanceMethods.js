2af9d4c41a55935aa4212658414e8f8a3952451a
Jessica Lawshe <jessica@jlawshe.com>
2016-02-12 13:10:38 -0600

    Advance Publish - insert callback

--- server/methods/advanceMethods.js
+++ server/methods/advanceMethods.js
@@ -1,5 +1,6 @@
 Meteor.methods({
 	advancePublish: function(list){
+        var fut = new future();
         var out = [];
         for (var i = 0; i < list.length; i++){
             var article = articles.findOne({'_id':list[i].article_id});
@@ -13,7 +14,14 @@ Meteor.methods({
                 name: 'advance'
                 ,pubtime: new Date
                 ,data: out
-            });
+        }, function (error, result) {
+            if(error){
+                throw new Meteor.Error(error);
+            }else if(result){
+                fut['return'](result);
+            }
+        });
+        return fut.wait();
 	}
 });
 
