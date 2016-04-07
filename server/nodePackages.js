13edff480bee8d21cc2721d6af9d8e9ca30fc8fc
Jessica Lawshe <jessica@jlawshe.com>
2016-04-07 11:32:38 -0500

    Added xmldom module back in
    xmldom is used to parse XML for HTML full text

--- server/nodePackages.js
+++ server/nodePackages.js
@@ -2,3 +2,4 @@ fs = Meteor.npmRequire('fs');
 parseString = Meteor.npmRequire('xml2js').parseString;
 future = Npm.require('fibers/future');
 xpath = Meteor.npmRequire('xpath');
+dom = Meteor.npmRequire('xmldom').DOMParser;
