7893255be91723a75f5c035c75fab9c1afc75858
Jessica Lawshe <jessica@jlawshe.com>
2016-05-06 12:32:17 -0500

    Typo fix

--- js/fxns/fxns.js
+++ js/fxns/fxns.js
@@ -632,12 +632,11 @@ Meteor.general = {
         if(string){
             string = string.replace(/<italic>/g,'<i>').replace(/<\/italic>/g,'</i>');
             string = string.replace(/(\r\n|\n|\r)/gm,''); // line breaks
-            string = string.replace(/\s+/g,' '); // remove extra spaces
+            string = string.replace(/\s+/g,''); // remove extra spaces
             if(string.charAt(string.length - 1) === '.'){
                 string = string.substring(0, string.length-1);
             }
         }
-
         return string;
     }
 }
