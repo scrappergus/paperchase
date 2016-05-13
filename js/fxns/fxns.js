ae5f1f0250476f37435d271009502165995e6ce0
Jessica Lawshe <jessica@jlawshe.com>
2016-05-13 14:50:35 -0500

    Article Files - linking supplemental

--- js/fxns/fxns.js
+++ js/fxns/fxns.js
@@ -49,15 +49,12 @@ Meteor.article = {
     linkFiles:function(files,articleMongoId){
         for(var file in files){
             if(files[file].file){
-                if(file != 'figures'){
-                    files[file].url =  journalConfig.findOne({}).assets + file + '/' + files[file].file;
-                }else{
-                    for(var fig in files[file]){
-                        files[file][fig].url =  journalConfig.findOne({}).assets + 'paper_figures/' + files[file][fig].file;
-                    }
+                files[file].url =  journalConfig.findOne({}).assets + file + '/' + files[file].file;
+            }else if(file === 'supplemental' || file === 'figures'){
+                for(var f in files[file]){
+                    files[file][f].url =  journalConfig.findOne({}).assets + 'supplemental_materials/' + files[file][f].file;
                 }
             }
-
         }
         files.journal = journalConfig.findOne({}).journal.short_name;
         files._id = articleMongoId;
