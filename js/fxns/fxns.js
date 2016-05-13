4bf1a57de1f874265aa98cb929c7792b0f0216d6
Jessica Lawshe <jessica@jlawshe.com>
2016-05-13 12:08:38 -0500

    Article Files

--- js/fxns/fxns.js
+++ js/fxns/fxns.js
@@ -48,13 +48,16 @@ Meteor.article = {
     },
     linkFiles:function(files,articleMongoId){
         for(var file in files){
-            if(file != 'figures'){
-                files[file].url =  journalConfig.findOne({}).assets + file + '/' + files[file].file;
-            }else{
-                for(var fig in files[file]){
-                    files[file][fig].url =  journalConfig.findOne({}).assets + 'paper_figures/' + files[file][fig].file;
+            if(files[file].file){
+                if(file != 'figures'){
+                    files[file].url =  journalConfig.findOne({}).assets + file + '/' + files[file].file;
+                }else{
+                    for(var fig in files[file]){
+                        files[file][fig].url =  journalConfig.findOne({}).assets + 'paper_figures/' + files[file][fig].file;
+                    }
                 }
             }
+
         }
         files.journal = journalConfig.findOne({}).journal.short_name;
         files._id = articleMongoId;
