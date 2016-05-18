f320002cd92f7bcdff8653eac7402a3e651b2c82
Gus Clemens <scrappergus@gmail.com>
2016-05-18 09:40:21 -0700

    Changed sorting order to reverse alphabetical per Misha’s request. We need to make this sorting something that can be managed via an interface.

--- paperchase.js
+++ paperchase.js
@@ -497,7 +497,7 @@ if (Meteor.isClient) {
         },
         data: function(){
             return {
-                eic: edboard.find({role:"Editor-in-Chief"},{sort : {name_last:1}}),
+                eic: edboard.find({role:"Editor-in-Chief"},{sort : {name_last:-1}}),
                 fullBoard: edboard.find({$or: [{role:"Founding Editorial Board"}, {role:"Editorial Board"}]},{sort : {name_last:1}})
             }
         }
