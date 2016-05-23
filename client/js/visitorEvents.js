782ab031da03e395dd79dd5ca89ff9ba874c365f
Amanda Saffer <amandasaffer@gmail.com>
2016-05-23 11:58:20 -0600

    fix wonky sidebar

--- client/js/visitorEvents.js
+++ client/js/visitorEvents.js
@@ -75,8 +75,7 @@ Template.Visitor.events({
     'click ul.nav-links a li': function(e){
         $('.sidebar-toggle-mobile').removeClass('icon-close');
         $('.sidebar-toggle-mobile').addClass('icon-menu');
-        // $('.content').toggleClass('no-sidebar');
-        $('.sidebar').toggleClass('mobile-show');
+        $('.sidebar').removeClass('mobile-show');
     },
     'click #mobile-search': function(e){
         Router.go('Search');
