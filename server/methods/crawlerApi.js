899fc97c6fa36185ea301516cc67287b2705a6c2
Jessica Lawshe <jessica@jlawshe.com>
2016-05-23 09:12:52 -0500

    dot notation

--- server/methods/crawlerApi.js
+++ server/methods/crawlerApi.js
@@ -15,7 +15,7 @@ Meteor.methods({
                     for(var a=0 ; a<articlesList.length ; a++){
                         Meteor.call('addArticle',articlesList[a],function(addError,articleAdded){
                             if(addError){
-                                console.error('addError: ' + articlesList[a]['pii'], addError);
+                                console.error('addError: ' + articlesList[a].pii, addError);
                             }else if(articleAdded){
                                 console.log('added: '+ articleAdded);
                             }
@@ -37,7 +37,7 @@ Meteor.methods({
         Meteor.http.get(requestURL, function(error,result){
             if(error){
                 console.error(error);
-                fut['throw'](error);
+                fut.throw(error);
                 throw new Meteor.Error(503, 'ERROR: DOI Registered Check' , error);
             }else if(result){
                 // combine with articles DB
@@ -45,9 +45,9 @@ Meteor.methods({
                 var articlesDoiList = JSON.parse(result.content);
 
                 for(var a=0 ; a<articlesDoiList.length ; a++){
-                    articlesDoiList[a]['paperchase'].doiRegisterUrl = registerURL + journalShortName + '/';
+                    articlesDoiList[a].paperchase.doiRegisterUrl = registerURL + journalShortName + '/';
                 }
-                fut['return'](articlesDoiList);
+                fut.return(articlesDoiList);
             }
         });
 
@@ -79,7 +79,7 @@ Meteor.methods({
                             }else if(updateResult){
                                 // console.log('updateResult',updateResult);
                                 if(i == parseInt(articlesList.length -1)){
-                                    fut['return'](articlesList.length);
+                                    fut.return(articlesList.length);
                                 }
                             }
                         });
@@ -88,7 +88,7 @@ Meteor.methods({
                 }else{
                     // No articles were updated
                     console.error('No article XML was updated in crawl');
-                    fut['return'](true);
+                    fut.return(true);
                 }
             }
         });
@@ -108,10 +108,10 @@ Meteor.methods({
                 articlesList = articlesListRes.data;
                 // console.log('articles = ',articlesList.length);
                 for(var i=0 ; i<articlesList.length ; i++){
-                    if(articlesList[i]['published']){
-                        var pii = articlesList[i]['idarticles'].toString();
-                        var epubDate = new Date(articlesList[i]['published'] + ' 00:00:00.0000');
-                        // console.log(pii + ': ' + articlesList[i]['published'] + ' : ' + epubDate);
+                    if(articlesList[i].published){
+                        var pii = articlesList[i].idarticles.toString();
+                        var epubDate = new Date(articlesList[i].published + ' 00:00:00.0000');
+                        // console.log(pii + ': ' + articlesList[i].published + ' : ' + epubDate);
                         articles.update({'ids.pii' : pii},{$set: {'dates.epub' :epubDate}});
                     }
                 }
@@ -126,25 +126,25 @@ Meteor.methods({
                 console.error('getCrossRefEpub/getAllArticlesDoiStatus', error);
             }else if(articlesList){
                 for(var i=0 ; i<articlesList.length ; i++){
-                    if(articlesList[i]['crossref_epub_date']){
-                        // console.log(articlesList[i]['crossref_epub_date']);
+                    if(articlesList[i].crossref_epub_date){
+                        // console.log(articlesList[i].crossref_epub_date);
                         // see if Paperchase DB is missing Epub, if so add this date.
-                        var articleInfo = articles.findOne(articlesList[i]['paperchase']['_id']);
-                        if(articleInfo && articleInfo['dates'] && !articleInfo['dates']['epub']){
+                        var articleInfo = articles.findOne(articlesList[i].paperchase._id);
+                        if(articleInfo && articleInfo.dates && !articleInfo.dates.epub){
                             console.log('MISSING DATE');
-                            var convertedDate = Meteor.dates.dashedToDate(articlesList[i]['crossref_epub_date']);
-                            // console.log('Update : ' + articlesList[i]['paperchase']['_id'] + ' / ' + convertedDate);
-                            articles.update({_id : articlesList[i]['paperchase']['_id']}, {$set: {'dates.epub' : convertedDate}},function(e,r){
+                            var convertedDate = Meteor.dates.dashedToDate(articlesList[i].crossref_epub_date);
+                            // console.log('Update : ' + articlesList[i].paperchase._id + ' / ' + convertedDate);
+                            articles.update({_id : articlesList[i].paperchase._id}, {$set: {'dates.epub' : convertedDate}},function(e,r){
                                 if(e){
                                     console.error('Update Article Error',e);
                                 }else if(r){
                                     console.log(r);
                                 }
                             });
-                        }else if(articleInfo && articleInfo['dates']){
-                            var convertedDate = Meteor.dates.dashedToDate(articlesList[i]['crossref_epub_date']);
-                            console.log('Update : ' + articlesList[i]['paperchase']['_id'] + ' / ' + convertedDate);
-                            articles.update({_id : articlesList[i]['paperchase']['_id']}, {$set: {'dates.epub' : convertedDate}},function(e,r){
+                        }else if(articleInfo && articleInfo.dates){
+                            var convertedDate = Meteor.dates.dashedToDate(articlesList[i].crossref_epub_date);
+                            console.log('Update : ' + articlesList[i].paperchase._id + ' / ' + convertedDate);
+                            articles.update({_id : articlesList[i].paperchase._id}, {$set: {'dates.epub' : convertedDate}},function(e,r){
                                 if(e){
                                     console.error('Update Article Error',e);
                                 }else if(r){
@@ -172,7 +172,7 @@ Meteor.methods({
                 console.error(error);
                 throw new Meteor.Error(503, 'ERROR: PDF to S3' , error);
             }else if(result){
-                fut['return'](true);
+                fut.return(true);
             }
         });
         return fut.wait();
@@ -214,9 +214,9 @@ Meteor.methods({
                     inDbUpdate.forEach(function(articleToUpdate){
                         var updateObj = {};
                         // console.log(articleToUpdate.ids);
-                        updateObj['ids.pmid'] = articleToUpdate.ids.pmid;
+                        updateObj.ids.pmid = articleToUpdate.ids.pmid;
                         if(articleToUpdate.ids.pmc){
-                            updateObj['ids.pmc'] = articleToUpdate.ids.pmc;
+                            updateObj.ids.pmc = articleToUpdate.ids.pmc;
                             console.log(updateObj);
                             Meteor.call('updateArticle', articleToUpdate.ids.mongo_id, updateObj,function(updateError,articleUpdated){
                                 if(updateError){
@@ -251,7 +251,7 @@ Meteor.methods({
                         throw new Meteor.Error(503, 'pubMedAndPmcAudit PMC' , pmcError);
                     }else if(pmcResult){
                         result.pmc = pmcResult.content;
-                        fut['return'](result);
+                        fut.return(result);
                     }
                 });
             }
@@ -270,13 +270,13 @@ Meteor.methods({
                 if(result.data.registered == 'Registered'){
                     Meteor.call('updateArticle', mongoId, {'ids.doi' : result.data.doi}, function(error,result){
                         if(result){
-                            fut['return']('Registered');
+                            fut.return('Registered');
                         }else if(error){
-                            fut['return'](error);
+                            fut.return(error);
                         }
                     });
                 }else{
-                    fut['return']('Not Registered');
+                    fut.return('Not Registered');
                 }
             }
         });
