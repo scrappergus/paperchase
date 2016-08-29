c00ad573817e50bc2ae216ec898edf9869ae8ad1
Jessica Lawshe <jessica@jlawshe.com>
2016-08-29 12:10:56 -0500

    dot notation

--- server/creds.js
+++ server/creds.js
@@ -2,39 +2,39 @@ Meteor.methods({
     getConfigSiteUrl : function(){
         var siteConfig = journalConfig.findOne({}, {fields: {journal : 1}});
         if(siteConfig){
-            return siteConfig['journal']['url'];
+            return siteConfig.journal.url;
         }
     },
     getConfigRecommendationEmail : function(){
         var emailConfig = journalConfig.findOne({}, {fields: {email_lib_recommendation : 1}});
         if(emailConfig){
             return {
-                'address' : emailConfig['email_lib_recommendation']['address'].replace('@','%40'),
-                'pw' : emailConfig['email_lib_recommendation']['pw']
+                'address' : emailConfig.email_lib_recommendation.address.replace('@','%40'),
+                'pw' : emailConfig.email_lib_recommendation.pw
             };
         }
     },
     getConfigRecommendationEmailAddress : function(){
         var emailConfig = journalConfig.findOne({}, {fields: {email_lib_recommendation : 1}});
         if(emailConfig){
-            return emailConfig['email_lib_recommendation']['address']; // do not replace @ here. Email.send does not require encoding
+            return emailConfig.email_lib_recommendation.address; // do not replace @ here. Email.send does not require encoding
         }
     },
     getConfigSubmission : function(){
         var submissionConfig = journalConfig.findOne({}, {fields: {submission : 1}});
         if(submissionConfig){
             return {
-                url: submissionConfig['submission']['url'],
-                username: submissionConfig['submission']['user'],
-                password: submissionConfig['submission']['pw'],
-                cookie : submissionConfig['submission']['cookie']
-            }
+                url: submissionConfig.submission.url,
+                username: submissionConfig.submission.user,
+                password: submissionConfig.submission.pw,
+                cookie : submissionConfig.submission.cookie
+            };
         }
     },
     getConfigJournal : function(){
         var settings = journalConfig.findOne({}, {fields: {journal : 1}});
         if(settings){
-            return settings['journal'];
+            return settings.journal;
         }
     },
 });
