50c90c28a7a4963ed206ad3e78f916b274f81053
Jessica Lawshe <jessica@jlawshe.com>
2016-11-18 10:32:21 -0600

    Conferences - Past by year

--- server/methods/newsMethods.js
+++ server/methods/newsMethods.js
@@ -2,20 +2,27 @@ Meteor.methods({
     conferencesPastAndFuture: function(articles){
         var today = new Date();
         var result = {};
-        result.past = [];
+        result.past = {};
         result.future = [];
 
         var conferences = newsList.find({display: true, conference: true}, {sort: {'conference_date_start': 1}}).fetch();
 
         conferences.forEach(function(conference){
             if ( conference.conference_date_start && conference.conference_date_start < today) {
-                result.past.push(conference);
+                var confDate =  new Date(conference.conference_date_start);
+                var confYear = confDate.getFullYear();
+                if (!result.past[confYear]) {
+                    result.past[confYear] = [];
+                }
+                result.past[confYear].push(conference);
             } else {
                 result.future.push(conference);
             }
         });
 
-        result.past.reverse();
+        for (var pastYear in result.past){
+            result.past[pastYear].reverse();
+        }
 
         return result;
     }
