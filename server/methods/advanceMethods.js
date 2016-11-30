b1380ef2efd3895e585d65e55c3b3cd6477d4f03
Jessica Lawshe <jessica@jlawshe.com>
2016-11-30 11:25:11 -0600

    Advance methods - dot notation

--- server/methods/advanceMethods.js
+++ server/methods/advanceMethods.js
@@ -1,23 +1,25 @@
 Meteor.methods({
     advancePublish: function(){
+        // for OJS
         var list = sorters.findOne({name:'advance'});
         list = list.articles;
         var out = [];
         for (var i = 0; i < list.length; i++){
             var article = list[i];
-            var section = sections.findOne({'section_id' : article['section_id']});
-              article['section_name'] = section['section_name'];
+            var section = sections.findOne({'section_id' : article.section_id});
+              article.section_name = section.section_name;
 
             out.push(article);
         }
 
         return publish.insert({
-                name: 'advance'
-                ,pubtime: new Date
-                ,data: out
+                name: 'advance',
+                pubtime: new Date(),
+                data: out
             });
     },
     orderBySectionId: function(articles){
+        // for OJS
         var byId = {};
         for(var i=0 ; i<articles.length ; i++){
             if(!byId[articles[i].section_id]){
